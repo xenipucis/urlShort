aaa84d72b24e5ce5f519f2dce69943d6a680ece0
Tiberiu Carp <carp_tiby@ymail.com>
2015-10-31 23:24:05 +0100

    Adding browsing functionality of the short generated id.

--- src/main/java/es/urlSh/controllers/URLController.java
+++ src/main/java/es/urlSh/controllers/URLController.java
@@ -1,7 +1,10 @@
 package es.urlSh.controllers;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.validator.routines.UrlValidator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +23,7 @@ public class URLController {
 	private StringRedisTemplate redis;
 	
 	@RequestMapping(value="/register", method=RequestMethod.GET) 
-	public String register(Map<String, Object> model) {
-		
+	public String register() {
 		return "register";
 	}
 	
@@ -32,9 +34,24 @@ public class URLController {
 		if (urlValidator.isValid(url)) {
 			final String id = Hashing.murmur3_32().hashString(url, StandardCharsets.UTF_8).toString();
 			model.addAttribute("url_short", id);
+			redis.opsForValue().set(id, url);
 			return "success";
 		} else {
 			return "errorRegister";
 		}
+	}
+	
+	@RequestMapping(value="/browse")
+	public String addUrlForBrowsing() {
+		return "browse";
+	}
+	
+	@RequestMapping(value="/browse", method=RequestMethod.POST) 
+	public void browseURL(String url, HttpServletResponse resp) throws IOException {
+		final String url_long = redis.opsForValue().get(url);
+		if (url_long != null)
+			resp.sendRedirect(url_long);
+		else
+			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
 	}
 }
