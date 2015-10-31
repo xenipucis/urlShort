d1137aab3a9a386990cd1ab633850d6db7fa68cf
Tiberiu Carp <carp_tiby@ymail.com>
2015-10-31 22:47:16 +0100

    Validate URL, based on supported protocols, using commons-validator from Apache; in case of an invalid URL, display an error message about that. Generate a 32 uuid identifier, corresponding to a valid url, using google guava core libraries.

--- src/main/java/es/urlSh/controllers/URLController.java
+++ src/main/java/es/urlSh/controllers/URLController.java
@@ -1,16 +1,23 @@
 package es.urlSh.controllers;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
+import org.apache.commons.validator.routines.UrlValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.google.common.hash.Hashing;
+
 @Controller
 public class URLController {
 
-	
+	@Autowired
+	private StringRedisTemplate redis;
 	
 	@RequestMapping(value="/register", method=RequestMethod.GET) 
 	public String register(Map<String, Object> model) {
@@ -21,6 +28,13 @@ public class URLController {
 	@RequestMapping(value="/register", method=RequestMethod.POST) 
 	public String saveURL(String url, Model model) {
 		model.addAttribute("url", url);
-		return "success";
+		final UrlValidator urlValidator = new UrlValidator(new String[]{"http", "https", "ftp"});
+		if (urlValidator.isValid(url)) {
+			final String id = Hashing.murmur3_32().hashString(url, StandardCharsets.UTF_8).toString();
+			model.addAttribute("url_short", id);
+			return "success";
+		} else {
+			return "errorRegister";
+		}
 	}
 }
